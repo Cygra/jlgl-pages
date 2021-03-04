044e275839e3000994b92dddd0e3105fa0613feb
Cygra <sjtuwbh@gmail.com>
2021-03-04 10:20:25 +0800

    feat: JLGL: New JLGL Page Directory done

--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,61 @@
-import * as vscode from "vscode";
-import * as fs from 'fs';
+import * as vscode from 'vscode'
+import * as fs from 'fs'
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand("jlgl.pages", async (name?) => {
+    vscode.commands.registerCommand('jlgl.pages', async () => {
       try {
-
-        if (!name || !name.path || !/pages\/src\/pages$/.test(name.path)) {
-          throw new Error("not the pages directory");
+        let path = vscode.workspace.workspaceFolders?.[0].uri.fsPath
+        if (!path) {
+          throw new Error('请打开项目文件夹')
         }
 
-        const input = await vscode.window.showInputBox({ prompt: 'please input directory' });
+        path += '/src/pages'
+
+        const input = await vscode.window.showInputBox({ prompt: '请输入页面路径' })
         if (!input) {
-          throw new Error("please input page name");
+          throw new Error('请输入页面路径')
         }
+        path += `/${input}`
 
-        fs.mkdirSync(`${name.path}/${input}`);
-        const title = await vscode.window.showInputBox({ prompt: 'please input title' });
+        fs.mkdirSync(path)
 
+        const title = await vscode.window.showInputBox({ prompt: '请输入页面 title' })
         if (!title) {
-          throw new Error("please input page name");
+          throw new Error('请输入页面 title')
         }
-        fs.writeFileSync(`${name.path}/${input}/page.config.json`,
+
+        fs.writeFileSync(`${path}/page.config.json`,
           [
-            `{`,
+            '{',
             `  "title": "${title}"`,
-            `}`
-          ].join('\n'));
-        fs.writeFileSync(`${name.path}/${input}/Page.vue`, [
-          `<template>`,
-          `  <div></div>`,
-          `</template>`,
-          `<script>`,
-          `export default {`,
-          `}`,
-          `</script>`,
-          `<style lang="scss">`,
-          `</style>`,
-        ].join('\n'));
-        fs.writeFileSync(`${name.path}/${input}/main.js`, [
-          `import Vue from 'vue'`,
-          `import Page from './Page.vue'`,
-          ``,
-          `new Vue({ render: h => h(Page) }).$mount('#app')`,
-        ].join('\n'));
-        vscode.window.showInformationMessage("done");
+            '}',
+            ''
+          ].join('\n'))
+        fs.writeFileSync(`${path}/Page.vue`, [
+          '<template>',
+          '  <div></div>',
+          '</template>',
+          '',
+          '<script>',
+          'export default {',
+          '}',
+          '</script>',
+          '',
+          '<style lang="scss">',
+          '</style>'
+        ].join('\n'))
+        fs.writeFileSync(`${path}/main.js`, [
+          'import Vue from \'vue\'',
+          'import Page from \'./Page.vue\'',
+          '',
+          'new Vue({ render: h => h(Page) }).$mount(\'#app\')',
+          ''
+        ].join('\n'))
+        vscode.window.showInformationMessage('done')
       } catch (error) {
-        vscode.window.showErrorMessage(error.message);
+        vscode.window.showErrorMessage(error.message)
       }
     })
-  );
+  )
 }
